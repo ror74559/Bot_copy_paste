468c0941bdbb41de917f080857eb4a4a6bc82d5c
Rafael Ribeiro <61093652+ror74559@users.noreply.github.com>
2020-10-29 18:18:10 -0300

    Adicionando arquivos
    O arquivo html eu utilizei só para marcar as coordenadas e inserir no script para o bot clicar em determinados botões da tela do google docs.

---
+++ index.js
@@ -0,0 +1,22 @@
+const scrape_images = require('./bots/scrape_images.js');
+
+const paste_images = require('./bots/paste_images.js');
+
+async function pasteImages(){
+	const paste = paste_images()
+}
+
+
+async function bookImages(){
+
+	const images = await scrape_images();
+
+	await console.log('Abrindo navegador para iniciar o redimensionamento e a colagem das imagens...')
+
+	await pasteImages()
+
+}
+
+bookImages()
+
+
